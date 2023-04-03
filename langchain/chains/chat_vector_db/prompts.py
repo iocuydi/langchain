6d660bf8502b4d2be2dbc14c8e95897425ac309b
iocuydi <46613640+iocuydi@users.noreply.github.com>
2023-04-02 18:46:04 -0700

    fix prompts for chat vector db
    make it less likely to ignore the new question if the new question is about a new topic

--- langchain/chains/chat_vector_db/prompts.py
+++ langchain/chains/chat_vector_db/prompts.py
@@ -1,11 +1,17 @@
 # flake8: noqa
 from langchain.prompts.prompt import PromptTemplate
 
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+_template = """Given the following conversation history and a new question, rephrase the new question to be a standalone question.
+If the follow up question is about a completely different topic and has nothing to do with the conversation, then the standalone question should just be the new question.
 
 Chat History:
 {chat_history}
-Follow Up Input: {question}
+
+New Question:
+Human: {question}
+
+(Remember that while the follow up question may be directly related to the chat history, it could also be a new question about a whole different topic.)
+
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
